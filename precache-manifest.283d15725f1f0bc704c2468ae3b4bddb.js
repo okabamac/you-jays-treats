7721d1f446873e1623d93dc9a55471340581056e
Okaba Mac <markokaba99@gmail.com>
2019-03-23 17:19:43 +0100

    Updates

---
+++ precache-manifest.283d15725f1f0bc704c2468ae3b4bddb.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f3d4149407423ced96c782c1a0f8150",
+    "url": "/you-jays-treats/static/media/logo.7f3d4149.jpg"
+  },
+  {
+    "revision": "6a42182392626b27d5bd",
+    "url": "/you-jays-treats/static/css/main.1c917ac8.chunk.css"
+  },
+  {
+    "revision": "34800e488e4a58ae1385",
+    "url": "/you-jays-treats/static/js/runtime~main.34800e48.js"
+  },
+  {
+    "revision": "1b7f50e93d785bbb9a535364c70253e0",
+    "url": "/you-jays-treats/static/media/bodyback.1b7f50e9.svg"
+  },
+  {
+    "revision": "3d25a9cba668857559a1",
+    "url": "/you-jays-treats/static/js/2.3d25a9cb.chunk.js"
+  },
+  {
+    "revision": "808fdea786c31ac40bb4128a939a01b5",
+    "url": "/you-jays-treats/static/media/kheigl.808fdea7.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/you-jays-treats/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/you-jays-treats/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/you-jays-treats/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/you-jays-treats/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/you-jays-treats/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e98023c7bdfc8be56c4a9ec1a18ea7aa",
+    "url": "/you-jays-treats/static/media/header.e98023c7.svg"
+  },
+  {
+    "revision": "6a42182392626b27d5bd",
+    "url": "/you-jays-treats/static/js/main.6a421823.chunk.js"
+  },
+  {
+    "revision": "45cb0d1208fa61eba6ac58f604e8fcba",
+    "url": "/you-jays-treats/static/media/cherish.45cb0d12.jpg"
+  },
+  {
+    "revision": "c3f5465b2fb72874141801cc6888eb52",
+    "url": "/you-jays-treats/static/media/pablo(1).c3f5465b.jpg"
+  },
+  {
+    "revision": "c36202704efe6d4b3f679d5e2e33ad67",
+    "url": "/you-jays-treats/static/media/pablo(2).c3620270.jpg"
+  },
+  {
+    "revision": "00c0d7c3868ca34cacfb13763172832d",
+    "url": "/you-jays-treats/static/media/pablo(3).00c0d7c3.jpg"
+  },
+  {
+    "revision": "f97dd3961460df6e4394bc7558e6ffaa",
+    "url": "/you-jays-treats/static/media/pablo(4).f97dd396.jpg"
+  },
+  {
+    "revision": "82190b2ae230221fe7e7bcba35268445",
+    "url": "/you-jays-treats/static/media/pablo(5).82190b2a.jpg"
+  },
+  {
+    "revision": "ee2dd31a93246fb72490c012b16eefca",
+    "url": "/you-jays-treats/static/media/pablo(6).ee2dd31a.jpg"
+  },
+  {
+    "revision": "b4580ad8942a5620144460aeffbd0ebf",
+    "url": "/you-jays-treats/static/media/landscape.b4580ad8.jpg"
+  },
+  {
+    "revision": "d34b534da05392e79361f65aa534321a",
+    "url": "/you-jays-treats/static/media/ninecakes.d34b534d.png"
+  },
+  {
+    "revision": "74dd94ad4f6fa4f3ad641c93722953e3",
+    "url": "/you-jays-treats/static/media/caramel.74dd94ad.jpg"
+  },
+  {
+    "revision": "3d25a9cba668857559a1",
+    "url": "/you-jays-treats/static/css/2.61e73f20.chunk.css"
+  },
+  {
+    "revision": "e95e07bccd53c09b49ff5fb447e462e8",
+    "url": "/you-jays-treats/index.html"
+  }
+];
